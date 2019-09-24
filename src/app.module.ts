b62044ff86de5833be3b245bffca1122b31e3be6
Ishpreet <ishpreet2000@gmail.com>
2019-09-24 14:30:37 -0500

    Created Campuses module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { LocationModule } from './location/location.module';
-import { ProductModule } from './product/product.module';
-import { LocationsModule } from './locations/locations.module';
-import { ProductsModule } from './products/products.module';
-import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [UsersModule, LocationModule, ProductModule, LocationsModule, ProductsModule, OrdersModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
