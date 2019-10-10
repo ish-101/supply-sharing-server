7cfb59cb71628ad54400fcf4a6ef2b2537de78a8
Ishpreet <ishpreet2000@gmail.com>
2019-10-09 23:54:52 -0500

    Update graphql.module.ts

--- src/graphql/graphql.module.ts
+++ src/graphql/graphql.module.ts
@@ -4,9 +4,12 @@ import { GraphQLModule } from '@nestjs/graphql';
 @Module({
     imports: [
         GraphQLModule.forRoot({
-            playground: true,
             autoSchemaFile: 'autogenSchema.gql',
             context: ({ req }) => ({ req }),
+            debug: true,
+            tracing: true,
+            introspection: true,
+            playground: true
         }),
     ],
 })
